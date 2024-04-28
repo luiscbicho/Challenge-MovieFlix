cce7ddfc682743b5d88c34f2702df59441cdc13f
luiscbicho <159563337+luiscbicho@users.noreply.github.com>
2024-04-28 15:46:58 +0200

    Final Commit - fix PageRequest

--- src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -8,7 +8,6 @@ import com.devsuperior.movieflix.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -36,11 +35,11 @@ public class MovieController {
     @PreAuthorize("hasAnyRole('ROLE_VISITOR','ROLE_MEMBER')")
     @GetMapping
     public ResponseEntity<Page<MovieCardDTO>> searchByGenre(@RequestParam(value = "page", defaultValue = "0") Integer page,
-                                                            @RequestParam(value = "linesPerPage", defaultValue = "5") Integer linesPerPage,
-                                                            @RequestParam(value = "orderBy", defaultValue = "title") String orderBy,
+                                                            @RequestParam(value = "size", defaultValue = "4") Integer size,
+                                                            @RequestParam(value = "sort", defaultValue = "title") String orderBy,
                                                             @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-                                                            @RequestParam(value = "genreId", defaultValue = "0") String genreId, Pageable pageable) {
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+                                                            @RequestParam(value = "genreId", defaultValue = "0") String genreId) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.valueOf(direction), orderBy);
         Page<MovieCardDTO> result = service.searchByGenre(genreId, pageRequest);
         return ResponseEntity.ok().body(result);
     }
